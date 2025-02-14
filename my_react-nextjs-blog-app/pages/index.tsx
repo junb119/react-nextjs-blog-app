3c511ae90acad2bdb83f038f6cc762636aaca5e7
jun <emfowkd@gmail.com>
2025-02-14 17:18:21 +0900

    1-8 getStaticProps를 이용한 포스트 리스트 나열

--- my_react-nextjs-blog-app/pages/index.tsx
+++ my_react-nextjs-blog-app/pages/index.tsx
@@ -1,7 +1,17 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import homeStyles from "../styles/Home.module.css";
-const Home: NextPage = () => {
+import { getSortedPostsData } from "../lib/post";
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
+  // console.log(allPostsData);
   return (
     <div>
       <Head>
@@ -13,9 +23,26 @@ const Home: NextPage = () => {
       </section>
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
         <h2 className={homeStyles.headingLg}>Blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
 };
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
